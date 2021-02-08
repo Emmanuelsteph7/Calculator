dc5c796b1d7aa8fbf5a69fdce5f1ad7a46e92a66
Emmanuelsteph7 <stephenemmanuel770@gmail.com>
2021-02-08 02:07:53 +0100

    build folder

---
+++ build/precache-manifest.04061e956a07cde6ff216897d1e25b5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b46ac354163cec760d0135dcbc3b51",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "f7cd7ea442376c3c5d62",
+    "url": "/Calculator/static/css/2.bb4ba17c.chunk.css"
+  },
+  {
+    "revision": "913af2e8c636f4e24cff",
+    "url": "/Calculator/static/css/main.3b3ddcc4.chunk.css"
+  },
+  {
+    "revision": "f7cd7ea442376c3c5d62",
+    "url": "/Calculator/static/js/2.5ca826c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.5ca826c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913af2e8c636f4e24cff",
+    "url": "/Calculator/static/js/main.928c5955.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  }
+]);
